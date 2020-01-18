18f1fd7196b919437cf66c0bef206a26db758d71
wonseok <wonseok5893@naver.com>
2020-01-18 22:10:15 +0900

    Header CSS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import express from "express"; // require() 찾아서 모듈 가져와 
-import morgan from "morgan";//logging middleware 또 helmet이 있음
+import express from "express"; // require() 찾아서 모듈 가져와
+import morgan from "morgan"; //logging middleware 또 helmet이 있음
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
@@ -11,10 +11,11 @@ import { localMiddleware } from "./localMiddleware";
 const app = express();
 
 app.use(helmet());
-app.set("view engine","pug");
-app.use("/uploads", express.static("uploads"))
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
